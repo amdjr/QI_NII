ee5f48c5a89d20d1b0b2dd05f1287fdaa56e6dcd
amdjr <adilson_monteiro@outlook.com>
2022-03-18 17:58:52 -0300

    comit start HomeScreen

--- GdbApp/src/pages/Gallery/index.js
+++ GdbApp/src/pages/Gallery/index.js
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
 
 function GalleryScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Galeria</Text>     
+      <Text>Galeria</Text>
     </View>
   );
 }
