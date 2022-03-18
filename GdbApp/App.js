96c5937175bcb20f68df581f281235672155e22f
amdjr <adilson_monteiro@outlook.com>
2022-03-17 22:01:17 -0300

    ok comit

--- GdbApp/App.js
+++ GdbApp/App.js
@@ -1,9 +1,9 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import HomeScreen from './src/pages/Home';
 import BioScreen from './src/pages/Bio';
@@ -13,23 +13,47 @@ import GalleryScreen from './src/pages/Gallery';
 import AboutScreen from './src/pages/About';
 import PlayerScreen from './src/pages/Player';
 
-
-
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Início' }}  />
-        <Stack.Screen name="Bio" component={BioScreen} options={{ title: 'Bio do Grupo' }} />
-        <Stack.Screen name="Splash" component={SplashScreen} options={{ title: 'splash' }}  />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Login' }}  />
-        <Stack.Screen name="About" component={AboutScreen} options={{ title: 'Sobre' }}  />
-        <Stack.Screen name="Gallery" component={GalleryScreen} options={{ title: 'Galeria' }}  />
-        <Stack.Screen name="Player" component={PlayerScreen} options={{ title: 'Player' }}  />
-        
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Início'}}
+        />
+        <Stack.Screen
+          name="Bio"
+          component={BioScreen}
+          options={{title: 'Bio do Grupo'}}
+        />
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{title: 'splash'}}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{title: 'Login'}}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{title: 'Sobre'}}
+        />
+        <Stack.Screen
+          name="Gallery"
+          component={GalleryScreen}
+          options={{title: 'Galeria'}}
+        />
+        <Stack.Screen
+          name="Player"
+          component={PlayerScreen}
+          options={{title: 'Player'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
